df2d3fe51dd706dba9b05f96acc05a48fef73eda
jasl <jasl9187@hotmail.com>
2021-07-10 03:21:28 +0800

    reload befaore calculate reward

--- app/models/remark_referrer_event.rb
+++ app/models/remark_referrer_event.rb
@@ -29,6 +29,7 @@ class RemarkReferrerEvent < ApplicationRecord
         next if referrer_contributor.referrer_id == contributor.id
 
         referrer_contributor.referrals << contributor
+        referrer_contributor.reload
         referrer_contributor.calculate_reward!
       end
 
